07690bb4efcd30abf41f0c49df07d65545d187d4
justjoe314 <justjoe314@gmail.com>
2016-02-04 20:04:29 -0600

    added info to sitemap and played with contact form

--- projects/biz/contact.php
+++ projects/biz/contact.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>Joe mckinley Practice page</title>	
-		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
-		<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<title>Joe mckinley Practice page</title>	
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+		
+		<link rel="icon" href="assets/fonts/favicon.ico" type="image/x-icon">
+		<!--favicon -->
 	</head>
 	
 	<body>
@@ -26,113 +30,85 @@
 			  <ul class="nav navbar-nav">
 				<li><a href="index.html">Home</a></li>
 				<li><a href="about.html">About</a></li>
-				<li class="active"><a href="contact.html">Contact</a></li>
 				<li><a href="products.html">Products</a></li>
+				<li class="active"><a href="contact.html">Contact</a></li>
 			  </ul>
 			</div><!--/.nav-collapse -->
 		  </div>
 		</nav>
-	<!--php email form-->
-	<?php
-	include "sendEmail.php"; 
-		<form name="contactform" method="post" action="send_form_email.php">
-			 
-			<table width="450px">
-			 
-			<tr>
-			 
-			 <td valign="top">
-			 
-			  <label for="first_name">First Name *</label>
-			 
-			 </td>
-			 
-			 <td valign="top">
-			 
-			  <input  type="text" name="first_name" maxlength="50" size="30">
-			 
-			 </td>
-			 
-			</tr>
-			 
-			<tr>
-			 
-			 <td valign="top"">
-			 
-			  <label for="last_name">Last Name *</label>
-			 
-			 </td>
-			 
-			 <td valign="top">
-			 
-			  <input  type="text" name="last_name" maxlength="50" size="30">
-			 
-			 </td>
-			 
-			</tr>
-			 
-			<tr>
-			 
-			 <td valign="top">
-			 
-			  <label for="email">Email Address *</label>
-			 
-			 </td>
-			 
-			 <td valign="top">
-			 
-			  <input  type="text" name="email" maxlength="80" size="30">
-			 
-			 </td>
-			 
-			</tr>
-			 
-			<tr>
-			 
-			 <td valign="top">
-			 
-			  <label for="telephone">Telephone Number</label>
-			 
-			 </td>
-			 
-			 <td valign="top">
-			 
-			  <input  type="text" name="telephone" maxlength="30" size="30">
-			 
-			 </td>
-			 
-			</tr>
-			 
-			<tr>
-			 
-			 <td valign="top">
-			 
-			  <label for="comments">Comments *</label>
-			 
-			 </td>
-			 
-			 <td valign="top">
-			 
-			  <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
-			 
-			 </td>
-			 
-			</tr>
-			 
-			<tr>
-			 
-			 <td colspan="2" style="text-align:center">
-			 
-			  <input type="submit" value="Submit">   <a href="http://www.freecontactform.com/email_form.php">Email Form</a>
-			 
-			 </td>
-			 
-			</tr>
-			 
-			</table>
-			 
-			</form>
-	?>
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-lg-7">
+			<!--php email form-->
+			<div class="contactbox">
+				
+				<form name="contactform" method="post" action="sendEmail.php">			 
+					<table width="450px">
+					<tr>
+					<td valign="top">
+					<label for="first_name">First Name *</label>
+					</td>
+					<td valign="top">
+					<input  type="text" name="first_name" maxlength="50" size="30">
+					</td>
+					</tr>
+					<tr>
+					<td valign="top"">
+					<label for="last_name">Last Name *</label>
+					</td>
+					<td valign="top">
+					<input  type="text" name="last_name" maxlength="50" size="30">
+					</td>
+					</tr>
+					<tr>
+					<td valign="top">
+					<label for="email">Email Address *</label>
+					 </td>
+					<td valign="top">
+					 <input  type="text" name="email" maxlength="80" size="30">
+					</td>
+					</tr>
+					<tr>
+					<td valign="top">
+					<label for="telephone">Telephone Number</label>	 
+					</td>
+					<td valign="top">
+					<input  type="text" name="telephone" maxlength="30" size="30">
+					</td>
+					</tr>
+					<tr>
+					 <td valign="top">
+					 <label for="comments">Comments *</label>
+					 </td>
+					 <td valign="top">
+					 <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
+					</td>
+					</tr>
+					<tr>
+					 <td colspan="2" style="text-align:center">
+					<input type="submit" value="Submit">   <a href="http://www.freecontactform.com/email_form.php">Email Form</a>
+					</td> 
+					</tr>
+					</table> 
+					</form> 
+						<?php
+						echo "<h2>Your Input:</h2>";
+						echo $name;
+						echo "<br>";
+						echo $email;
+						echo "<br>";
+						echo $website;
+						echo "<br>";
+						echo $comment;
+						echo "<br>";
+						echo $gender;
+						?>	
+					</div>
+				</div>
+			<div class="col-xs-12 col-lg-5"><p></p></div>
+		</div>	
+	</div>
 	</body>
 	
 </html>	
